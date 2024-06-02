fd68433f792e85792de444fd0f6e9f9adc8094eb
FelipeDellutri <felipedellutri@gmail.com>
2024-06-02 17:34:09 -0300

    Update BloquesAcero.java

--- Apellido1-Apellido2-Apellido3-tp-1/src/juego/BloquesAcero.java
+++ Apellido1-Apellido2-Apellido3-tp-1/src/juego/BloquesAcero.java
@@ -11,25 +11,34 @@ public class BloquesAcero {
 	private int y;
 	private final double angulo;
 	private double diametro;
+	private int ancho;
+	private int alto;
 	public BloquesAcero(Image img, int x, int y, double angulo, double diametro) {
 		this.img = Herramientas.cargarImagen("Imagenes\\acero.png");
 		this.x = x;
 		this.y = y;
 		this.angulo = angulo;
 		this.diametro = diametro;
+		//Tamaño
+		this.ancho = 160;
+		this.alto = 60;
 	}
 	public void dibujarImg(Entorno entorno) {
 		entorno.dibujarImagen(this.img, this.x, this.y, angulo, diametro);
 	}
-	
 	public Image getImg() {
 		return img;
 	}
 	public int getX() {
 		return x;
 	}
-
 	public int getY() {
 		return y;
+	}
+	public int getAncho() {
+		return ancho;
+	}
+	public int getAlto() {
+		return alto;
 	}
 }
