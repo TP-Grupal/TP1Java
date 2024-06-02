272d34fac747f303566072b8cbc990ef381ea403
FelipeDellutri <felipedellutri@gmail.com>
2024-06-02 17:37:04 -0300

    Update BloquesLadrillos.java

--- Apellido1-Apellido2-Apellido3-tp-1/src/juego/BloquesLadrillos.java
+++ Apellido1-Apellido2-Apellido3-tp-1/src/juego/BloquesLadrillos.java
@@ -1,6 +1,5 @@
 package juego;
 
-import java.awt.Color;
 import entorno.Entorno;
 import entorno.Herramientas;
 import java.awt.Image;
@@ -11,26 +10,34 @@ public class BloquesLadrillos {
 	private int y;
 	private final double angulo;
 	private double diametro;
+	private int ancho;
+	private int alto;
 	public BloquesLadrillos(Image img, int x, int y, double angulo, double diametro) {
 		this.img = Herramientas.cargarImagen("Imagenes\\ladrillo.png");
 		this.x = x;
 		this.y = y;
 		this.angulo = angulo;
 		this.diametro = diametro;
+		// Tamaño
+		this.alto = 60;
+		this.ancho = 80;
 	}
 	public void dibujarImg(Entorno entorno) {
 		entorno.dibujarImagen(this.img, this.x, this.y, angulo, diametro);
 	}
-	
 	public Image getImg() {
 		return img;
 	}
 	public int getX() {
 		return x;
 	}
-
 	public int getY() {
 		return y;
 	}
-	
+	public int getAncho() {
+		return ancho;
+	}
+	public int getAlto() {
+		return alto;
+	}
 }
