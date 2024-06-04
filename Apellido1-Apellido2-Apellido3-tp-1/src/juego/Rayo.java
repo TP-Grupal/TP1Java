7c73732fbd56cabb5371b5fe2920a2970c46168c
Cristian <47560615+turbina31@users.noreply.github.com>
2024-06-04 14:26:12 -0300

    Update Rayo.java

--- Apellido1-Apellido2-Apellido3-tp-1/src/juego/Rayo.java
+++ Apellido1-Apellido2-Apellido3-tp-1/src/juego/Rayo.java
@@ -9,6 +9,8 @@ public class Rayo {
 	private Image img;
 	private int x;
 	private int y;
+	private int ancho;
+	private int alto;
 	private int velocidad;
 	private double angulo;
 	private double escala;
@@ -20,6 +22,8 @@ public class Rayo {
 		this.angulo = angulo;
 		this.escala = escala;
 		this.velocidad= velocidad;
+		this.ancho = 20;
+		this.alto = 20;
 	}
 	public void dibujarImg(Entorno entorno) {
 		entorno.dibujarImagen(this.img,this.x,this.y,this.angulo,this.escala);
@@ -45,6 +49,12 @@ public class Rayo {
 
 	public int getY() {
 		return y;
+	}
+	public int getAncho() {
+		return ancho;
+	}
+	public double getEscala() {
+		return escala;
 	}
 	public double getEscala() {
 		return escala;
